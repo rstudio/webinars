d732f3856e7c3dbf8359c3645bb47d956c50175f
Edgar Ruiz <edgararuiz@gmail.com>
2018-07-19 10:22:22 -0500

    Few tweaks to code and deck

--- 59-comunicando-con-R/5-r-fabricantes.R
+++ 59-comunicando-con-R/5-r-fabricantes.R
@@ -5,6 +5,13 @@ load("datos/millas.rda")
 
 fabricantes <- unique(millas$fabricante)
 
+# map() para correr la misma funcion por cada valor
+map(fabricantes, ~print(.x))
+
+# render() para crear un nuevo reporte
+render("4-rmarkdown-fabricante.Rmd", params = list(fabricante = "audi"))
+
+# map() y render() son una poderosa combinacion 
 map(fabricantes, ~ 
     render(
       "4-rmarkdown-fabricante.Rmd", 
--- 59-comunicando-con-R/9-plumber-api/plumber.R
+++ 59-comunicando-con-R/9-plumber-api/plumber.R
@@ -5,6 +5,10 @@ library(plumber)
 #* @get /millas
 #* @param cilindros
 #* @param clase
+
+load("datos/millas.rda")
+load("modelo.rds")
+
 function(cilindros, clase) {
   cilindros <- as.integer(cilindros)
   predict(modelo, data.frame(clase = clase, cilindros = cilindros))
